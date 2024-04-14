b001de5591616c688146dcf1ab142ef93fe7d3b6
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-04-14 15:49:58 +0200

    Add initial code for infoobject-details-main component

--- angular-elements/src/app/features/infoobject-details/infoobject-details-main.component.ts
+++ angular-elements/src/app/features/infoobject-details/infoobject-details-main.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-infoobject-details-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './infoobject-details-main.component.html',
-  styleUrl: './infoobject-details-main.component.scss'
+  styleUrl: './infoobject-details-main.component.scss',
 })
 export class InfoobjectDetailsMainComponent {
+  @Input() deleteUrl: string = '';
+  @Input() editUrl: string = '';
+  @Input() content: string = '';
+  @Input() topic: string = '';
+  @Input() authorIp: string = '';
+  @Input() tag: string = '';
+  @Input() dialogueContent: string = '';
+  @Input() infoObjectLinks: string[] = [];
+  @Input() todoContent: string = '';
 
+  copyToClipboard(content: string) {
+    navigator.clipboard.writeText(content).then(
+      () => console.log('Content copied!'),
+      (err) => console.error('Failed to copy: ', err)
+    );
+  }
+
+  deleteInfoObject() {
+    // Perform HTTP DELETE request or handle via parent component
+    console.log('Delete URL: ', this.deleteUrl);
+  }
 }
