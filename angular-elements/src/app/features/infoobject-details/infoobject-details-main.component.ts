6407817a420a533c95f86dfd856d15c7f43c975c
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-04-14 15:40:19 +0200

    Add component for infoobject-details

---
+++ angular-elements/src/app/features/infoobject-details/infoobject-details-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infoobject-details-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './infoobject-details-main.component.html',
+  styleUrl: './infoobject-details-main.component.scss'
+})
+export class InfoobjectDetailsMainComponent {
+
+}
