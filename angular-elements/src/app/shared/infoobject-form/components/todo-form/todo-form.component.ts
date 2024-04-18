32907cc8c1a12ee84ffd5ee34f5372930c6537de
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-04-18 21:58:46 +0200

    Add todoform component for infoobject

--- angular-elements/src/app/shared/infoobject-form/components/todo-form/todo-form.component.ts
+++ angular-elements/src/app/shared/infoobject-form/components/todo-form/todo-form.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { InfoobjectFormStoreService } from '../../services/infoobject-form-store.service';
 
 @Component({
   selector: 'app-todo-form',
   standalone: true,
   imports: [],
   templateUrl: './todo-form.component.html',
-  styleUrl: './todo-form.component.scss'
+  styleUrls: ['./todo-form.component.scss'],
 })
-export class TodoFormComponent {
+export class TodoFormComponent implements OnInit {
+  form: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private formStore: InfoobjectFormStoreService
+  ) {
+    this.form = this.formStore.getForm();
+  }
+
+  ngOnInit() {
+    if (!this.todosFormArray) {
+      this.form.setControl('todos', this.fb.array([]));
+    }
+  }
+
+  get todosFormArray(): FormArray {
+    return this.form.get('todos') as FormArray;
+  }
+
+  addTodo() {
+    const newTodoGroup = this.fb.group({
+      id: Date.now(),
+      content: '',
+      isDone: false,
+    });
+    this.todosFormArray.push(newTodoGroup);
+  }
+
+  removeTodo(index: number) {
+    this.todosFormArray.removeAt(index);
+  }
+
+  moveTodo(index: number, direction: 'up' | 'down') {
+    const currentIndex = index;
+    const newIndex = direction === 'up' ? index - 1 : index + 1;
+    if (newIndex < 0 || newIndex >= this.todosFormArray.length) return;
+
+    const currentGroup = this.todosFormArray.at(currentIndex);
+    this.todosFormArray.removeAt(currentIndex);
+    this.todosFormArray.insert(newIndex, currentGroup);
+  }
 }
