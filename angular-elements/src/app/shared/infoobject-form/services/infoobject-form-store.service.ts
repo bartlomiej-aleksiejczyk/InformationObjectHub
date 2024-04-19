2d292b7c4d05a9be5a129261a6d771898886cb41
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-04-19 22:10:02 +0200

    Update infoobject form store service with proper todo handling

--- angular-elements/src/app/shared/infoobject-form/services/infoobject-form-store.service.ts
+++ angular-elements/src/app/shared/infoobject-form/services/infoobject-form-store.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { Todo } from '../components/todo-form/utils/todo';
 
 @Injectable({
@@ -31,6 +31,28 @@ export class InfoobjectFormStoreService {
     tag?: string;
     todos?: Todo[];
   }): void {
-    this.form.patchValue(values);
+    this.form.patchValue({
+      content: values.content,
+      topic: values.topic,
+      tag: values.tag,
+    });
+
+    this.setTodos(values.todos);
+  }
+  private setTodos(todos?: Todo[]): void {
+    const todoArray = this.form.get('todos') as FormArray;
+    todoArray.clear(); // Clear existing entries in the form array
+
+    if (todos) {
+      todos.forEach((todo) => {
+        todoArray.push(
+          this.formBuilder.group({
+            id: todo.id,
+            content: todo.content,
+            isDone: todo.isDone,
+          })
+        );
+      });
+    }
   }
 }
