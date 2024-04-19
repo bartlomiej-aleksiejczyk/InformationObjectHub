47fe7644de163e071b4eb1b01d910c990f408071
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-04-19 22:12:39 +0200

    Alter infoobject todo form component

--- angular-elements/src/app/shared/infoobject-form/components/todo-form/todo-form.component.ts
+++ angular-elements/src/app/shared/infoobject-form/components/todo-form/todo-form.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { InfoobjectFormStoreService } from '../../services/infoobject-form-store.service';
 
 @Component({
   selector: 'app-todo-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './todo-form.component.html',
   styleUrls: ['./todo-form.component.scss'],
 })
@@ -13,24 +20,33 @@ export class TodoFormComponent implements OnInit {
   form: FormGroup;
 
   constructor(
-    private fb: FormBuilder,
+    private formBuilder: FormBuilder,
     private formStore: InfoobjectFormStoreService
   ) {
     this.form = this.formStore.getForm();
   }
 
   ngOnInit() {
-    if (!this.todosFormArray) {
-      this.form.setControl('todos', this.fb.array([]));
+    this.ensureTodosFormArray();
+  }
+
+  ensureTodosFormArray() {
+    if (!this.form.get('todos')) {
+      this.form.setControl('todos', this.formBuilder.array([]));
     }
+    console.log(this.form.controls['todos'].value);
   }
 
   get todosFormArray(): FormArray {
     return this.form.get('todos') as FormArray;
   }
 
+  getFormGroup(control: AbstractControl): FormGroup {
+    return control as FormGroup;
+  }
+
   addTodo() {
-    const newTodoGroup = this.fb.group({
+    const newTodoGroup = this.formBuilder.group({
       id: Date.now(),
       content: '',
       isDone: false,
