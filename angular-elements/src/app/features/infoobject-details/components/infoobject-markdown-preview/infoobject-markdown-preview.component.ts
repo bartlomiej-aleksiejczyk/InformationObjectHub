2b1d662eba7da95e65550ced668aacdf8e9d10b6
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-04-21 20:31:56 +0200

    Generate empty component for infoobject markdown preview

---
+++ angular-elements/src/app/features/infoobject-details/components/infoobject-markdown-preview/infoobject-markdown-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infoobject-markdown-preview',
+  standalone: true,
+  imports: [],
+  templateUrl: './infoobject-markdown-preview.component.html',
+  styleUrl: './infoobject-markdown-preview.component.scss'
+})
+export class InfoobjectMarkdownPreviewComponent {
+
+}
