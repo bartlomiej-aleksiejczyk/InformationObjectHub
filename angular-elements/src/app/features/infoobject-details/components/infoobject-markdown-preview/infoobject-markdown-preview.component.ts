c3f865bd50efa498446b705cab206b09ad7b9686
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-04-22 09:19:12 +0200

    Add separate component for markdown content

--- angular-elements/src/app/features/infoobject-details/components/infoobject-markdown-preview/infoobject-markdown-preview.component.ts
+++ angular-elements/src/app/features/infoobject-details/components/infoobject-markdown-preview/infoobject-markdown-preview.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MarkdownDisplayUtils } from '../../utils/MarkdownDisplayUtils';
+import { CopyableContentWrapperComponent } from "../../../../shared/copyable-content-wrapper/copyable-content-wrapper.component";
 
 @Component({
   selector: 'app-infoobject-markdown-preview',
   standalone: true,
-  imports: [],
   templateUrl: './infoobject-markdown-preview.component.html',
-  styleUrl: './infoobject-markdown-preview.component.scss'
+  styleUrl: './infoobject-markdown-preview.component.scss',
+  imports: [CopyableContentWrapperComponent]
 })
-export class InfoobjectMarkdownPreviewComponent {
+export class InfoobjectMarkdownPreviewComponent implements OnInit {
+  @Input() markdownContent: string = '';
+  renderedMarkdown: string = '';
+  constructor() {
+    console.log(this.markdownContent)
 
+  }
+  ngOnInit(): void {
+    this.renderedMarkdown = MarkdownDisplayUtils.parseMarkdownToHTML(this.markdownContent)
+    console.log(this.renderedMarkdown)
+
+  }
 }
