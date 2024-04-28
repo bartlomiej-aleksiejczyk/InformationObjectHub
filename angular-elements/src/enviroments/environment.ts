369848edd505230f1aff738f4bcbfd8c8b48a19b
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-04-28 17:17:12 +0200

    Fix enviroment.ts file in angular app

--- angular-elements/src/enviroments/environment.ts
+++ angular-elements/src/enviroments/environment.ts
@@ -1,5 +1,5 @@
 //Jenkins must replace this file
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://localhost:8080/api/v1',
+  apiBaseUrl: 'https://localhost:8080/springboot-info-object-hub/api/v1',
 };
