f6a0e7541dc1bed984cea6baf014b9bb8b58f8f5
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-05-04 14:15:21 +0200

    Configure  thick client for SPA mode

--- angular-thick-client/src/app/app.component.ts
+++ angular-thick-client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InfoobjectListComponent } from './features/infoobject-list/infoobject-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InfoobjectListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
