a394812a39dcf64f5f3725faf30c9d3b89540214
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-05-07 01:36:14 +0200

    Fix routing in spa client to redirect to more informative URL

--- angular-thick-client/src/app/app.routes.ts
+++ angular-thick-client/src/app/app.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
-export const routes: Routes = [
-    { path: 'app', component: AppComponent },
-    { path: '', redirectTo: '/app', pathMatch: 'full' },
-    { path: '**', redirectTo: '/app' }
-];
+export const routes: Routes = [];
